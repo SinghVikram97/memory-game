4eba8aaf98063eee6b45d2ee5bba78d4642a108d
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2018-11-16 12:16:37 +0530

    Enabled clicking on 2 cards

--- js/app.js
+++ js/app.js
@@ -36,11 +36,17 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+// Can't open more than 2 cards at a time
 let openCards=[];
 let allCards=document.querySelectorAll('.card');
 allCards.forEach(function (card) {
     card.addEventListener('click',function (event) {
-        openCards.push(card);
-        card.classList.add('show','open');        
+        if(openCards.length<2){
+          openCards.push(card);
+          card.classList.add('show','open');
+        }
+        else{
+            // Don't do anything
+        }        
     })
 })
